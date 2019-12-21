8487edb520fccdbf91f877696c1c2e80b70ca99c
Kamzs <kamilambroziak@interia.pl>
2019-12-21 13:09:48 +0100

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NestedCompComponent } from './nested-comp/nested-comp.component';
+import { ContentCompComponent } from './content-comp/content-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NestedCompComponent,
+    ContentCompComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/nested-comp/nested-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nested-comp',
+  templateUrl: './nested-comp.component.html',
+  styleUrls: ['./nested-comp.component.css']
+})
+export class NestedCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
