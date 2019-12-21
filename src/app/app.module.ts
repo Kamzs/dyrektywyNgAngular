05aed8a6c430b07e0375d9ea5a6402944a9207ae
Kamzs <kamilambroziak@interia.pl>
2019-12-21 17:34:11 +0100

    added numering using index in ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NestedCompComponent } from './nested-comp/nested-comp.component';
 import { ContentCompComponent } from './content-comp/content-comp.component';
+import { DyrektywyNGComponent } from './dyrektywy-ng/dyrektywy-ng.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NestedCompComponent,
-    ContentCompComponent
+    ContentCompComponent,
+    DyrektywyNGComponent
   ],
   imports: [
     BrowserModule
